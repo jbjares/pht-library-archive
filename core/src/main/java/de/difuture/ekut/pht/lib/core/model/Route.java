2df4b5ed3eb372b6985293a1da1201534bba3820
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-04-23 17:15:23 +0200

    [REFACTOR] Use edge set instead of edge list

--- core/src/main/java/de/difuture/ekut/pht/lib/core/model/Route.java
+++ core/src/main/java/de/difuture/ekut/pht/lib/core/model/Route.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 
@@ -24,7 +25,7 @@ import java.util.UUID;
 public final class Route {
 
     @JsonProperty("edges")
-    private List<Edge> edges;
+    private Set<Edge> edges;
 
     @Data
     @NoArgsConstructor
