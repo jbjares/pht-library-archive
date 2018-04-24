331938b8d481d84990a15314f4e4af8e71a987a4
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-04-24 19:15:25 +0200

    [REFACTOR] TrainTag is now String

--- core/src/main/java/de/difuture/ekut/pht/lib/core/messages/TrainUpdate.java
+++ core/src/main/java/de/difuture/ekut/pht/lib/core/messages/TrainUpdate.java
@@ -1,9 +1,6 @@
 package de.difuture.ekut.pht.lib.core.messages;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import de.difuture.ekut.pht.lib.core.traintag.TrainTag;
-import de.difuture.ekut.pht.lib.core.messages.deserializer.TrainUpdateDeserializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,7 +19,6 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonDeserialize(using = TrainUpdateDeserializer.class)
 public final class TrainUpdate {
 
 	// ID of the updated train. This is the same as the Docker Repository
@@ -35,5 +31,7 @@ public final class TrainUpdate {
 
     // The trainTag that the Docker Image (train) was published under
     @JsonProperty("trainTag")
-    private TrainTag trainTag;
+    //@JsonDeserialize(using = TrainTagDeserializer.class)
+    // TODO The above deserialzier does not work
+    private String trainTag;
 }
