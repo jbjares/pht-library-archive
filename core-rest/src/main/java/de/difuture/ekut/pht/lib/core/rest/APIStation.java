0805e07aab6acd9c54a0beff0a3e0db7d580ed33
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-06-04 14:01:46 +0200

    Improve doc of APIStation and APITrain

--- core-rest/src/main/java/de/difuture/ekut/pht/lib/core/rest/APIStation.java
+++ core-rest/src/main/java/de/difuture/ekut/pht/lib/core/rest/APIStation.java
@@ -11,6 +11,9 @@ import java.net.URI;
 
 /**
  *
+ * Represents a minimal response of an RESTful API that is supposed to return station
+ * entities.
+ *
  * @author Lukas Zimmermann
  */
 @Data
@@ -18,13 +21,26 @@ import java.net.URI;
 @AllArgsConstructor
 public final class APIStation {
 
+    /**
+     *  The global ID of the station. Knowledge of this ID is required for
+     *  purpose of communicating with RESTful services about this station.
+     *
+     */
     @JsonProperty("stationID")
+    @NotNull
     private Long stationID;
 
+    /**
+     * The clear text name of this station.
+     */
     @JsonProperty("stationName")
     @NotNull
     private String stationName;
 
+    /**
+     *  The URI of this station. Communication with the station in the context of the
+     *  Personal Health Train infrastructure must be performed using this station URI.
+     */
     @JsonProperty("stationURI")
     @NotNull
     private URI stationURI;
--- core-rest/src/main/java/de/difuture/ekut/pht/lib/core/rest/APITrain.java
+++ core-rest/src/main/java/de/difuture/ekut/pht/lib/core/rest/APITrain.java
@@ -5,10 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.net.URI;
 
 
 /**
+ *
+ * Represents a minimal response of an RESTful API that is supposed to return train
+ * entities.
  *
  * @author Lukas Zimmermann
  */
@@ -17,9 +21,40 @@ import java.net.URI;
 @NoArgsConstructor
 public final class APITrain {
 
+    /**
+     *  The global ID of the train. Knowledge of this ID is required for the following purposes
+     *  (a) communication with a RESTful API to perform requests about this train.
+     *  (b) Communication with the Train Docker Registry hosting this particular train.
+     *  The repository name in this Docker Registry hosting versions of the train is
+     *  identical to this trainID.
+     *
+     */
     @JsonProperty("trainID")
+    @NotNull
     private Long trainID;
 
+    /**
+     *  The Docker Registry URI hosting this train. Each train can only be associated
+     *  with at most Train Docker Registry. This value can be null for (at least) two
+     *  reasons.
+     *
+     *  (a) The train has not yet been associated with a Train Docker Registry.
+     *  (b) The services responding with objects of {@link APITrain} cannot determine
+     *  the URI of the Docker Train Registry containing this train.
+     *
+     */
     @JsonProperty("trainRegistryURI")
     private URI trainRegistryURI;
+
+
+    /**
+     * Returns whether this {@link APITrain} has a Train Registry URI.
+     *
+     *
+     * @return Whether this {@link APITrain} has a Train Registry URI.
+     */
+    public boolean hasTrainRegistryURI() {
+
+     return this.trainRegistryURI != null;
+    }
 }
